30b3e83b9a52c82d5da136ef4499795c5a75a9d9
KAISAR TOKTARKOZHA <kaisar403575@gmail.com>
2024-11-29 00:09:29 +0500

    first commit

---
+++ src/main/java/com/andersen/java_intensive_13/hotel_app/HotelAppApplication.java
@@ -0,0 +1,13 @@
+package com.andersen.java_intensive_13.hotel_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAppApplication.class, args);
+	}
+
+}
